80fb9795c3d2d0fa3e2ef308b7fa3f5cdb8b6a09
Viacheslav <viacheslavlitvinov@gmail.com>
2017-11-03 15:22:12 +0700

    Add whoIs

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def start(bot, update):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+
+
 def echo(bot, update):
 	if "саша" in str.lower(update.message.text):
 		bot.send_message(chat_id=update.message.chat_id, text="Так все мы знаем, что Саша - козявка.")
@@ -25,5 +27,19 @@ def echo(bot, update):
 
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
+
+
+def whois(bot, update, args):
+	reply = "Даже не знаю, что ответить. :("
+	if "саша" in str.lower(args):
+		reply = "Так все мы знаем, что Саша - козявка."
+	elif "слава" in str.lower(args):
+		reply = "Ну а Слава - молодец."
+
+	bot.send_message(chat_id=update.message.chat_id, text=reply)
+
+whois_handler = CommandHandler('whois', whois, pass_args=True)
+dispatcher.add_handler(whois_handler)
+
 
 updater.start_polling()
