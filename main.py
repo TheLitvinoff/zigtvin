52dfe322b53cd82cc67ddb1672ef5fb9d40a1735
Viacheslav <viacheslavlitvinov@gmail.com>
2017-11-03 15:08:00 +0700

    Add message reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import telegram
-from telegram.ext import Updater, CommandHandler 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 updater = Updater(token="479323920:AAHzN1pOgSvUlCSfdNr8ALBXUT0gMHLI6pM")
 dispatcher = updater.dispatcher
@@ -15,6 +15,15 @@ def start(bot, update):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+def echo(bot, update):
+	if "саша" in str.lower(update.message.text):
+		bot.send_message(chat_id=update.message.chat_id, text="Так все мы знаем, что Саша - козявка.")
+	else if "слава" in str.lower(update.message.text):
+		bot.send_message(chat_id=update.message.chat_id, text="Ну а Слава - молодец.")
+	else:
+		bot.send_message(chat_id=update.message.chat_id, text="Даже не знаю, что ответить. :(")
 
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
